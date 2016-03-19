8666bf84acbdb03962602101e386e735b7354318
one-more <greyjoi@yandex.ru>
2016-03-19 20:30:26 +0300

    code style

--- configuration/theme-light.js
+++ configuration/theme-light.js
@@ -6,23 +6,24 @@ let process = require('process'),
     stylesPath = themePath+'/styles',
     viewsPath = themePath+'/js/views';
 
+// try/catch because when include this file on the client, there is no such modules
 try {
     var lazypipe = require('lazypipe'),
         stylus = require('gulp-stylus'),
         nib = require('nib'),
         bundleOptions = {
-        minifyTemplates: true,
-        watchTemplates: true,
-        minifyStyles: true,
-        watchStyles: true,
-        transforms: {
-            styles: lazypipe().pipe(stylus, {
-                include: [themePath+'/includes/stylus'],
-                import: ['_variables', 'nib'],
-                use: nib()
-            })
-        }
-    };
+            minifyTemplates: true,
+            watchTemplates: true,
+            minifyStyles: true,
+            watchStyles: true,
+            transforms: {
+                styles: lazypipe().pipe(stylus, {
+                    include: [themePath+'/includes/stylus'],
+                    import: ['_variables', 'nib'],
+                    use: nib()
+                })
+            }
+        };
 } catch(e) {
     bundleOptions = {}
 }
