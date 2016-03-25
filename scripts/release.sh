4f672f324b35b33f2dd8887801053f43ca38dd4d
one-more <greyjoi@yandex.ru>
2016-03-25 07:47:57 +0300

    bash scripts

---
+++ scripts/release.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+OPTIND=1
+version='0.0.0'
+
+while getopts ":v:" opt; do
+    case "$opt" in
+    v)
+        version=$OPTARG
+    ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+    ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+echo "creating release branch with version $version"
+
+git co -b "release_$version" origin/dev
+git rm "TODO.md"
+git rm "README.md"
+git rm ".foreverignore"
+git rm "mocha-bootstrap.js"
+git rm --cached -r "scripts"
+git rm -r "test"
+git rm -r "tests-client"
+
+./build-static.sh
+
+git rm ./.gitignore
+
+git mv ./.masterignore ./.gitignore
+
+find ./static/images -name "*upload*" -type f -delete
+
+git add static/*
+
+git ci -m "release $version"
+git push -u origin "release_$version"
+
+git co dev
+
